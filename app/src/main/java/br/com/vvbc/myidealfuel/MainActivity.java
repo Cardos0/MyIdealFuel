959d40424b2c480cfc3d972970e325e5814b5d5a
Cardos0 <vinicius518vi@gmail.com>
2018-09-29 01:16:46 -0300

    Hotfix 1.0.0

--- app/src/main/java/br/com/vvbc/myidealfuel/MainActivity.java
+++ app/src/main/java/br/com/vvbc/myidealfuel/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.vvbc.myidealfuel;
 
+import android.graphics.Typeface;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,9 +8,14 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
+    private NumberFormat currencyFormat = NumberFormat.getCurrencyInstance();
     private TextInputEditText fuelIdealTextInputEditText;
+    private TextView gasValueTextView;
+    private TextView ethanolValueTextView;
     private double gasValue;
     private double ethanolValue;
     private ImageView fuelImageView;
@@ -19,11 +25,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // FindViews
+        gasValueTextView = findViewById(R.id.gasValueTextView);
+        ethanolValueTextView = findViewById(R.id.ethanolValueTextView);
         fuelIdealTextInputEditText = findViewById(R.id.fuelIdealTextInputEditText);
         fuelImageView = findViewById(R.id.fuelImageView);
-        calcFuelIdeal();
-
         SeekBar gasSeekBar = findViewById(R.id.gasSeekBar);
+        SeekBar ethanolSeekBar = findViewById(R.id.ethanolSeekBar);
+
+        gasValue = gasSeekBar.getProgress();
+        ethanolValue = ethanolSeekBar.getProgress();
+
         gasSeekBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
                     @Override
@@ -43,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
-        SeekBar ethanolSeekBar = findViewById(R.id.ethanolSeekBar);
+
         ethanolSeekBar.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
                     @Override
@@ -63,27 +75,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+        // Calc first time.
+        calcFuelIdeal();
     }
 
     private void calcFuelIdeal(){
         String fuelIdealName;
         Double fuelDivision;
+        String gasValueString = currencyFormat.format(gasValue);
+        String ethanolValueString = currencyFormat.format(ethanolValue);
         int imageSrc;
 
+        // Avoid Division by 0
         if (gasValue != 0){
             fuelDivision = ethanolValue / gasValue;
             if (fuelDivision >= 0.7){
                 fuelIdealName = getString(R.string.gas);
                 imageSrc = R.drawable.gas;
+                gasValueString = "--> " + gasValueString;
+                gasValueTextView.setTypeface(null, Typeface.BOLD);
+                ethanolValueTextView.setTypeface(null, Typeface.NORMAL);
             }else{
                 fuelIdealName = getString(R.string.ethanol);
                 imageSrc = R.drawable.ethanol;
+                ethanolValueString = "--> " + ethanolValueString;
+                ethanolValueTextView.setTypeface(null, Typeface.BOLD);
+                gasValueTextView.setTypeface(null, Typeface.NORMAL);
+
             }
         }else{
             fuelIdealName = getString(R.string.gas);
             imageSrc = R.drawable.gas;
+            gasValueString = "--> " + gasValueString;
+            gasValueTextView.setTypeface(null, Typeface.BOLD);
+            ethanolValueTextView.setTypeface(null, Typeface.NORMAL);
+
         }
-        fuelIdealTextInputEditText.setText(fuelIdealName);
-        fuelImageView.setImageResource(imageSrc);
+
+        // TextView of SeekBars
+        gasValueTextView.setText(gasValueString);
+        ethanolValueTextView.setText(ethanolValueString);
+
+        fuelIdealTextInputEditText.setText(fuelIdealName);  // Set texts
+        fuelImageView.setImageResource(imageSrc);           // Set image
     }
 }
